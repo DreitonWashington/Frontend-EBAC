307e2a07942002311998d0e01bcb74746c923185
Dreiton Washington <dreiton.washington@gmail.com>
2022-04-23 18:08:53 -0300

    Atualizacao

--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -1,24 +1,37 @@
+const { series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
+const babel = require('gulp-babel')
+const htmlmin = require('gulp-htmlmin')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
-function tarefasCss(cb){
-    return gulp.src(['./vendor/**/*.css', './vendor/bootstrap-5.1.3-dist/css/bootstrap.min.css', './src/css/style.css'])
+function tarefasCss(callback){
+    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css', './src/css/style.css', './vendor/owl/css/owl.css'])
         .pipe(concat('libs.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
+
+    return callback()
 }
 
-function tarefasJs(){
-    return gulp.src(['./src/js/**/*.js'])
+function tarefasJs(callback){
+    gulp.src(['./src/js/**/*.js'])
+        // .pipe(babel({
+        //     comments: false,
+        //     presets: ['@babel/env']
+        // }))
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/js'))
+
+        return callback()
 }
 
 function tarefaImages(){
@@ -37,6 +50,25 @@ function tarefaImages(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+function tarefasHtml(callback){
+
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+}
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./dist/**/*').on('change',reload)
+})
+
+exports.default = parallel(tarefasHtml, tarefasCss, tarefasJs)
 exports.styles = tarefasCss
 exports.scripts = tarefasJs
 exports.image = tarefaImages
